be10cc4b0882b6368c765d548c56ee671a905f8a
ishmam00 <93041311+ishmam00@users.noreply.github.com>
2023-08-12 21:07:19 +0600

    Modified some files

--- frontEnd/src/App.js
+++ frontEnd/src/App.js
@@ -1,51 +1,49 @@
-import React, {useState, useMemo} from 'react'
-import styled from "styled-components";
-import bg from './img/bg.png'
-import {MainLayout} from './styles/Layouts'
-import Orb from './Components/Orb/Orb'
-import Navigation from './Components/Navigation/Navigation'
+import React, { useState, useMemo } from 'react';
+import styled from 'styled-components';
+import bg from './img/bg.png';
+import { MainLayout } from './styles/Layouts';
+import Orb from './Components/Orb/Orb';
+import Navigation from './Components/Navigation/Navigation';
 import Dashboard from './Components/Dashboard/Dashboard';
 import Transactions from './Components/Transactions/Transactions';
-import Income from './Components/Income/Income'
+import Income from './Components/Income/Income';
 import Expenses from './Components/Expenses/Expenses';
 import Tax from './Components/Tax/Tax';
 import { useGlobalContext } from './context/globalContext';
 
 function App() {
-  const [active, setActive] = useState(1)
+  const [active, setActive] = useState(1);
 
-  const global = useGlobalContext()
+  const global = useGlobalContext();
   console.log(global);
 
   const displayData = () => {
-    switch(active){
+    switch (active) {
       case 1:
-        return <Dashboard />
+        return <Dashboard />;
       case 2:
-        return <Transactions />
+        return <Transactions />;
       case 3:
-        return <Income />
-      case 4: 
-        return <Expenses />
-      case 5: 
-        return <Tax />
-      default: 
-        return <Dashboard />
+        return <Income />;
+      case 4:
+        return <Expenses />;
+      case 5:
+        return <Tax />;
+      default:
+        return <Dashboard />;
     }
-  }
+  };
 
   const orbMemo = useMemo(() => {
-    return <Orb />
-  },[])
+    return <Orb />;
+  }, []);
 
   return (
     <AppStyled bg={bg} className="App">
       {orbMemo}
       <MainLayout>
         <Navigation active={active} setActive={setActive} />
-        <main>
-          {displayData()}
-        </main>
+        <main>{displayData()}</main>
       </MainLayout>
     </AppStyled>
   );
@@ -53,16 +51,16 @@ function App() {
 
 const AppStyled = styled.div`
   height: 100vh;
-  background-image: url(${props => props.bg});
+  background-image: url(${(props) => props.bg});
   position: relative;
-  main{
+  main {
     flex: 1;
     background: rgba(252, 246, 249, 0.78);
-    border: 3px solid #FFFFFF;
+    border: 3px solid #ffffff;
     backdrop-filter: blur(4.5px);
     border-radius: 32px;
     overflow-x: hidden;
-    &::-webkit-scrollbar{
+    &::-webkit-scrollbar {
       width: 0;
     }
   }
